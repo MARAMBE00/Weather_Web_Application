5122fd59f08d69685e8a47bf37ce3c149ec4a238
MARAMBE00 <pasindurmarambe00@gmail.com>
2023-05-30 23:49:27 +0530

    Current weather API integration

--- assets/js/module.js
+++ assets/js/module.js
@@ -32,7 +32,7 @@ export const monthNames = [
 */
 export const getDate = function(dateUnix, timezone){
     const date = new Date((dateUnix + timezone) * 1000);
-    const weekDayName = weekDayNames[date.getUTCDate()];
+    const weekDayName = weekDayNames[date.getUTCDay()];
     const monthName = monthNames[date.getUTCMonth()];
 
     return `${weekDayName} ${date.getUTCDate()}, ${monthName}`;
