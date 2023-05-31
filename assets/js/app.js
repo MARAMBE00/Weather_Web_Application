9807931c6bd8805572f1122f590e787427ce616f
MARAMBE00 <pasindurmarambe00@gmail.com>
2023-05-31 11:19:45 +0530

    1.0 beta version

--- assets/js/app.js
+++ assets/js/app.js
@@ -344,7 +344,7 @@ export const updateWeather = function (lat, lon){
                             <p class="title-2">${parseInt(temp_max)}&deg;</p>
                         </span>
                     </div>
-                    <p class="label-1">${date.getDate()} ${module.monthNames[date.getUTCMonth()]}</p>
+                    <p class="label-1">${date.getDate()} ${module.monthNames[date.getMonth()]}</p>
                     <p class="label-1">${module.weekDayNames[date.getUTCDay()]}</p>
                 `;
                 forecastSection.querySelector("[data-forecast-list]").appendChild(li);
